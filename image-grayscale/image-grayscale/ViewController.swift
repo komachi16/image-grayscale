54bc57662d11a508d0ffacd7616bae5457d1d1d3
komachi16 <haru16.nhs@gmail.com>
2024-09-29 16:36:16 +0900

    init project

---
+++ image-grayscale/image-grayscale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  image-grayscale
+//
+//  Created by komachi16 on 2024/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ image-grayscale/image-grayscaleTests/image_grayscaleTests.swift
@@ -0,0 +1,17 @@
+//
+//  image_grayscaleTests.swift
+//  image-grayscaleTests
+//
+//  Created by komachi16 on 2024/09/29.
+//
+
+import Testing
+@testable import image_grayscale
+
+struct image_grayscaleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
