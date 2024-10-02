1ac284876fe7e0a8e58d4f79a40c4a728ff09b13
komachi16 <haru16.nhs@gmail.com>
2024-10-03 01:32:00 +0900

    シャッター機能を追加

--- image-grayscale/image-grayscale/Presentation/Camera/CameraViewController.swift
+++ image-grayscale/image-grayscale/Presentation/Camera/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVFoundation
+import SnapKit
 
 class CameraViewController: UIViewController {
 
@@ -17,14 +18,50 @@ class CameraViewController: UIViewController {
 
     private var previewLayer: AVCaptureVideoPreviewLayer?
 
+    private lazy var shutterButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(shutterButtonTapped(_:)), for: .touchUpInside)
+        button.clipsToBounds = true
+        button.imageView?.contentMode = .scaleAspectFill
+        button.contentHorizontalAlignment = .fill
+        button.contentVerticalAlignment = .fill
+        button.tintColor = .white
+        button.setImage(UIImage(systemName: "camera.circle.fill"), for: .normal)
+        return button
+    }()
+
+    private let countdownLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        label.font = .systemFont(ofSize: 30)
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCamera()
+        setupLayout()
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         resetCamera()
+    }
+
+    private func setupLayout() {
+        view.addSubview(shutterButton)
+        view.addSubview(countdownLabel)
+
+        shutterButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-40)
+            $0.width.height.equalTo(72)
+        }
+
+        countdownLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.height.equalTo(64)
+        }
     }
 
     private func setupCamera() {
@@ -71,5 +108,45 @@ class CameraViewController: UIViewController {
         }
         previewLayer?.removeFromSuperlayer()
         previewLayer = nil
+    }
+
+    private func takePhoto() {
+        let settings = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+
+    @objc
+    private func shutterButtonTapped(_ sender: UIButton) {
+        countdownLabel.isHidden = false
+        startCountdown()
+    }
+
+    private func startCountdown() {
+        var countdown = 3
+        countdownLabel.text = "\(countdown)"
+
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
+            countdown -= 1
+            self?.countdownLabel.text = "\(countdown)"
+            if countdown == 0 {
+                self?.countdownLabel.isHidden = true
+                timer.invalidate()
+                self?.takePhoto()
+            }
+        }
+    }
+}
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(
+        _ output: AVCapturePhotoOutput,
+        didFinishProcessingPhoto photo: AVCapturePhoto,
+        error: Error?
+    ) {
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        let image = UIImage(data: imageData)
+        // TODO: モノクロ変換機能を追加
+
+        // TODO: 結果画面に遷移
     }
 }
