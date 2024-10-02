5c5beeee68b45c2c95137cfd5093508bb6a20aa9
komachi16 <haru16.nhs@gmail.com>
2024-10-02 22:50:37 +0900

    カメラの機能を追加

--- image-grayscale/image-grayscale/Presentation/Camera/CameraViewController.swift
+++ image-grayscale/image-grayscale/Presentation/Camera/CameraViewController.swift
@@ -6,13 +6,70 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    private let captureSession = AVCaptureSession()
+
+    private var deviceInput: AVCaptureDeviceInput?
+    private var photoOutput = AVCapturePhotoOutput()
+
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCamera()
     }
 
-}
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        resetCamera()
+    }
 
+    private func setupCamera() {
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+        } catch let error as NSError {
+            print("Error setting up camera input: \(error)")
+            return
+        }
+
+        guard let deviceInput = deviceInput else { return }
+
+        if captureSession.canAddInput(deviceInput) {
+            captureSession.addInput(deviceInput)
+        } else {
+            return
+        }
+
+        photoOutput = AVCapturePhotoOutput()
+
+        if captureSession.canAddOutput(photoOutput) {
+            captureSession.addOutput(photoOutput)
+        } else {
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.frame = view.layer.bounds
+        previewLayer?.videoGravity = .resizeAspectFill
+        
+        if let previewLayer = previewLayer {
+            view.layer.addSublayer(previewLayer)
+        }
+
+        captureSession.startRunning()
+    }
+
+    private func resetCamera() {
+        captureSession.stopRunning()
+        if let deviceInput = deviceInput {
+            captureSession.removeInput(deviceInput)
+        }
+        previewLayer?.removeFromSuperlayer()
+        previewLayer = nil
+    }
+}
